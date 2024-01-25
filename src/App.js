3e58081f758ced8eb8ab573bad426a924f9d2e56
surendra <surendrareddy.nara@gmail.com>
2024-01-25 22:55:49 +0530

    jenkins commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Hello jenkins
+          Hello jenkins automated code CI/CD pipeline
         </p>
         <a
           className="App-link"
